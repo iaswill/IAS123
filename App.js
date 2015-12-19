000fe38cb10ade6303b20888b2e62d3918ab360a
iaswill <wantjoule@incomm.com>
2015-12-19 14:53:12 +0000

    First commit to IAS with text to console

---
+++ App.js
@@ -0,0 +1,9 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    launch: function() {
+        //Write app code here
+            console.log('Our First Version Has Arrived!');
+        //API Docs: https://help.rallydev.com/apps/2.0/doc/
+    }
+});
